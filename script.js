e62eab0b255a976188476a34f70e9fd8395e41ad
Silky Ng <silky.ng@gmail.com>
2023-01-28 14:39:03 +0000

    typing text logic

---
+++ script.js
@@ -0,0 +1,45 @@
+let words = ['Full Stack Developer', 'Runner', 'Pianist'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 100;
+
+const wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substring(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    document.getElementById('typingText').innerHTML = part;
+  },speed);
+};
+
+wordflick();
